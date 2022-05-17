4d02a473589d321d141f4be35e3a339397d9d597
Asel <asel.alimbaeva.kk@gmail.com>
2022-05-17 18:54:49 +0300

    feat: code for folder 04 ()

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
 
+const fs = require('fs');
+const path = require('path');
+const fsPromises = fs.promises;
+
+
+const pathFile = path.join(path.join(__dirname), 'files-copy');
+fs.stat(pathFile, (error) => {
+  if (error) {
+    console.log('error');
+    fsPromises.mkdir(pathFile).then(function () {
+      console.log('Directory created successfully');
+    }).catch(function () {
+      console.log('failed to create directory');
+    });
+
+  }
+  copy();
+});
+
+
+function copy() {
+  fs.readdir(path.join(__dirname, 'files'), (err, data) => {
+    data.forEach(file => {
+      let f1 = path.join(__dirname, 'files', `${file}`);
+      let f2 = path.join(__dirname, 'files-copy', `${file}`);
+      try {
+        fsPromises.copyFile(f1, f2);
+        console.log('Good job');
+      } catch {
+        console.log('The file could not be copied');
+      }
+    });
+  });
+}
+
